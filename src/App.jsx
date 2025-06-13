f6eefec1f9cc69a8f68b6c38b7c6b58fc0710511
Enzo <dsoftware76@px.com.br>
2025-06-13 16:26:48 -0300

    refact: info button on current team and type images

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,50 @@ import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react'
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInAnonymously, onAuthStateChanged, signInWithCustomToken } from 'firebase/auth';
 import { getFirestore, collection, doc, getDoc, setDoc, onSnapshot, deleteDoc, query } from 'firebase/firestore';
+import NormalIcon from "./assets/typeIcons/Normal_icon_LA.png";
+import FireIcon from "./assets/typeIcons/Fire_icon_LA.png";
+import WaterIcon from "./assets/typeIcons/Water_icon_LA.png";
+import ElectricIcon from "./assets/typeIcons/Electric_icon_LA.png";
+import GrassIcon from "./assets/typeIcons/Grass_icon_LA.png";
+import IceIcon from "./assets/typeIcons/Ice_icon_LA.png";
+import FightingIcon from "./assets/typeIcons/Fighting_icon_LA.png";
+import PoisonIcon from "./assets/typeIcons/Poison_icon_LA.png";
+import GroundIcon from "./assets/typeIcons/Ground_icon_LA.png";
+import FlyingIcon from "./assets/typeIcons/Flying_icon_LA.png";
+import PsychicIcon from "./assets/typeIcons/Psychic_icon_LA.png";
+import BugIcon from "./assets/typeIcons/Bug_icon_LA.png";
+import RockIcon from "./assets/typeIcons/Rock_icon_LA.png";
+import GhostIcon from "./assets/typeIcons/Ghost_icon_LA.png";
+import DragonIcon from "./assets/typeIcons/Dragon_icon_LA.png";
+import DarkIcon from "./assets/typeIcons/Dark_icon_LA.png";
+import SteelIcon from "./assets/typeIcons/Steel_icon_LA.png";
+import FairyIcon from "./assets/typeIcons/Fairy_icon_LA.png";
 
 // --- Assets & Data ---
 const POKEBALL_PLACEHOLDER_URL = 'https://art.pixilart.com/sr2a947c8f967b8.png';
 const COLORS = { primary: '#7d65e1', background: '#111827', card: '#1F2937', cardLight: '#374151', text: '#FFFFFF', textMuted: '#9CA3AF' };
 const typeColors = { normal: '#A8A77A', fire: '#EE8130', water: '#6390F0', electric: '#F7D02C', grass: '#7AC74C', ice: '#96D9D6', fighting: '#C22E28', poison: '#A33EA1', ground: '#E2BF65', flying: '#A98FF3', psychic: '#F95587', bug: '#A6B91A', rock: '#B6A136', ghost: '#735797', dragon: '#6F35FC', dark: '#705746', steel: '#B7B7CE', fairy: '#D685AD' };
 const typeIcons = {
-    normal: "https://archives.bulbagarden.net/media/upload/c/cb/Normal_icon_LA.png?20220213053429", fire: "https://archives.bulbagarden.net/media/upload/4/48/Fire_icon_LA.png?20220213053749", water: "https://archives.bulbagarden.net/media/upload/5/5e/Water_icon_LA.png?20220213053800", electric: "https://archives.bulbagarden.net/media/upload/7/75/Electric_icon_LA.png?20220213053826", grass: "https://archives.bulbagarden.net/media/upload/1/1b/Grass_icon_LA.png?20220213053815", ice: "https://archives.bulbagarden.net/media/upload/7/70/Ice_icon_LA.png?20220213053853", fighting: "https://archives.bulbagarden.net/media/upload/6/68/Fighting_icon_LA.png?20220213053440", poison: "https://archives.bulbagarden.net/media/upload/b/b4/Poison_icon_LA.png?20220213053545", ground: "https://archives.bulbagarden.net/media/upload/4/45/Ground_icon_LA.png?20220213053610", flying: "https://archives.bulbagarden.net/media/upload/d/de/Flying_icon_LA.png?20220213053533", psychic: "https://archives.bulbagarden.net/media/upload/4/45/Psychic_icon_LA.png?20220213053708", bug: "https://archives.bulbagarden.net/media/upload/2/26/Bug_icon_LA.png?20220213053628", rock: "https://archives.bulbagarden.net/media/upload/8/85/Rock_icon_LA.png?20220213053620", ghost: "https://archives.bulbagarden.net/media/upload/b/b5/Ghost_icon_LA.png?20220213053637", dragon: "https://archives.bulbagarden.net/media/upload/2/28/Dragon_icon_LA.png?20220213053915", dark: "https://archives.bulbagarden.net/media/upload/7/7f/Dark_icon_LA.png?20220213053921", steel: "https://archives.bulbagarden.net/media/upload/f/f9/Steel_icon_LA.png?20220213053740", fairy: "https://archives.bulbagarden.net/media/upload/b/b1/Fairy_icon_LA.png?20220213053936"
+    normal: NormalIcon, 
+    fire: FireIcon, 
+    water: WaterIcon, 
+    electric: ElectricIcon, 
+    grass: GrassIcon, 
+    ice: IceIcon, 
+    fighting: FightingIcon, 
+    poison: PoisonIcon, 
+    ground: GroundIcon, 
+    flying: FlyingIcon, 
+    psychic: PsychicIcon, 
+    bug: BugIcon, 
+    rock: RockIcon, 
+    ghost: GhostIcon, 
+    dragon: DragonIcon, 
+    dark: DarkIcon, 
+    steel: SteelIcon, 
+    fairy: FairyIcon
 };
+
 const typeChart = {
     normal: { damageTaken: { Fighting: 2, Ghost: 0 }, damageDealt: { Rock: 0.5, Steel: 0.5 } },
     fire: { damageTaken: { Water: 2, Ground: 2, Rock: 2, Fire: 0.5, Grass: 0.5, Ice: 0.5, Bug: 0.5, Steel: 0.5, Fairy: 0.5 }, damageDealt: { Fire: 0.5, Water: 0.5, Rock: 0.5, Dragon: 0.5, Grass: 2, Ice: 2, Bug: 2, Steel: 2 } },
@@ -214,10 +250,16 @@ const TeamBuilderView = ({
 
                         <button
                         onClick={(e) => { e.stopPropagation(); showDetails(p); }}
-                        className="absolute top-1 left-1 bg-gray-700 bg-opacity-50 text-white rounded-full h-5 w-5 flex items-center justify-center transition-opacity text-sm"
-                        >
+                        className={`
+                        absolute top-2 left-2 text-gray-400 hover:text-white transition-colors
+                        opacity-100     
+                        lg:opacity-0    
+                        lg:group-hover:opacity-100 
+                        lg:transition-opacity
+                        `}
+                    >
                         <InfoIcon />
-                        </button>
+                    </button>
 
                         <button
                         onClick={(e) => { e.stopPropagation(); handleRemoveFromTeam(p.id); }}
